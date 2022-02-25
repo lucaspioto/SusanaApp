6926ca7f0ed96e6b043db5412f32067c2722fa4c
Lucas Tamagnone <lucaspioto@gmail.com>
2022-02-25 09:53:33 -0300

    First Commit

---
+++ SusanaApp/SusanaApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SusanaApp
+//
+//  Created by Lucas on 25/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+      
+      print("Entrou na ViewController")
+   }
+
+
+}
+
